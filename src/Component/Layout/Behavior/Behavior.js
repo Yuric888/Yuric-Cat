df4d662669e7ea4d2fd49c34311bb9b7c4b61198
Trung Nguyễn <yuricdev@gmail.com>
2022-08-21 00:26:30 +0700

    Change-behavior-1: Yuric-1

--- src/Component/Layout/Behavior/Behavior.js
+++ src/Component/Layout/Behavior/Behavior.js
@@ -1,31 +1,32 @@
+import { useEffect, useState } from 'react'
 import './Behavior.css'
-import CatUnplash from '../../Images_Svg/Cat-unplash-1.svg'
-const DataBehavior = [{
-  id: 1,
-  url: "https://i.postimg.cc/9FLjBVny/Cat-behavior-4.jpg",
-  title: 'Cat Behavior 1',
-  content: 'dl;sfah;lshfd'
-},{
-  id: 2,
-  url: "https://i.postimg.cc/V6SH8Pf0/Cat-behavior-3.jpg",
-  title: 'Cat Behavior 2',
-  content: '.k,j'
-},{
-  id: 3,
-  url: "https://sieupet.com/sites/default/files/tap_tinh_cua_meo6.jpg",
-  title: 'Cat Behavior 3',
-  content: 'dgfc'
-},]
+import DataBehavior from './DataBehavior.js'
 const Behavior = () => {
+  const [state, setState] = useState({activeState: true})
+  useEffect(() =>{
+    const handleChange = () => {
+      let scroll = window.pageYOffset;
+      console.log('scroll', scroll)
+  }
+  window.addEventListener('scroll', handleChange)
+
+  return window.removeEventListener('scroll', handleChange)
+  })
+  
   return (
     <div className="wrapper-behavior">
-      <div className="container-behavior">
-          <div className="content-behavior">
-          <img atl="" src={CatUnplash} />
-           <div className="info-behavior">
-            </div>
+      
+      {DataBehavior && DataBehavior.map((item, idx) => {
+        return(
+          <div key={idx} style={{backgroundImage: `url(${item.url})`}} className="content-behavior">
+           <div className="container-behavior">
+           <p className={state.activeState === true ? "text-1 active" : "text-1"}>{item.p1}</p>
+           <p className={state.activeState === true ? "text-2 active" : "text-2"}>{item.p2}</p>
+           <p className={state.activeState === true ? "text-3 active" : "text-3"}>{item.p3}</p>
           </div>
-      </div>
+          </div>
+        )
+      })}
     </div>
   )
 }
---
+++ src/Component/Layout/Behavior/DataBehavior.js
@@ -0,0 +1,20 @@
+const DataBehavior = [{
+  id: 1,
+  url: "https://raw.githubusercontent.com/ThanhTrung05/react-hook-basic-with-yuric/main/Cat-behavior-svg-1.svg",
+  p1: 'Cat Behavior 1',
+  p2: 'Cat Behavior 2',
+  p3: 'Cat Behavior 3'
+},{
+  id: 2,
+  url: "https://raw.githubusercontent.com/ThanhTrung05/react-hook-basic-with-yuric/main/Cat-behavior-svg-2.svg",
+  p1: 'Cat Behavior 3',
+  p2: 'Cat Behavior 4',
+  p3: 'Cat Behavior 3'
+},{
+  id: 3,
+  url: "https://raw.githubusercontent.com/ThanhTrung05/react-hook-basic-with-yuric/main/Cat-behavior-svg-6.svg",
+  p1: 'Cat Behavior 5',
+  p2: 'Cat Behavior 6',
+  p3: 'Cat Behavior 3'
+},]
+export default DataBehavior
