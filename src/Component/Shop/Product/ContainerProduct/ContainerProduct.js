051745ab83be7d28de65047ae778f6eef598490b
Trung Nguyễn <yuricdev@gmail.com>
2022-08-28 14:43:00 +0700

    Create-component-list-product: Yuric

--- src/Component/Shop/Product/ContainerProduct/ContainerProduct.js
+++ src/Component/Shop/Product/ContainerProduct/ContainerProduct.js
@@ -1,58 +1,13 @@
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
 import './ContainerProduct.css'
-import DataProduct from './dataProduct'
-import CatImg from '../../../Images_Svg/Cat-unplash-1.svg'
-import IconPlus from '../../../Images_Svg/IconPlus';
-import IconShop from '../../../Images_Svg/IconShop';
+import DataProduct from './dataProduct';
+import ListItemProduct from './ListItemProduct/ListItemProduct';
 const ContainerProduct = () => {
    const showLayout = useSelector((state) => state.reduce.showLayout)
-  // const dispatch = useDispatch()
   return (
     <div className="wrapper-list">
       <div className="container-list">
-    {/* {DataProduct && DataProduct} */}
-        <div className={showLayout && showLayout === true ? "content-layout-list" : "content-layout-grid"}>
-          {DataProduct && DataProduct.map((item, idx) => {
-            return(
-            <div className="card-product" key={idx}>
-            <div className="card-image">
-              <img alt={item.title} src={item.url ? item.url : "https://raw.githubusercontent.com/ThanhTrung05/react-hook-basic-with-yuric/main/Cat-himalayan(svg).svg"} />
-              <div className="card-image-button">
-                <div className="icon-plus">
-                  <IconPlus color="#666" width="22" height="22"/>
-                </div>
-                <div className="icon-shop">
-                  <IconShop color="#666" width="22" height="22" />
-                </div>
-              </div>
-            </div>
-            <div className="card-content-grid">
-              <h1 className="card-content-title">{item.title ? item.title : ""}</h1>
-              <div className="card-content-price">
-                <p>${item.price_2 ? `${item.price_1} - ` : item.price_1}</p>
-                <p className="card-content-line-through">{item.price_2 ? ` $${item.price_2}` : ""}</p>
-              </div>
-            </div>
-            <div className="card-content-list">
-              <h1 className="card-content-title">{item.title ? item.title : ""}</h1>
-              <div className="card-content-price">
-                <p>${item.price_2 ? `${item.price_1} - ` : item.price_1}</p>
-                <p className="card-content-line-through">{item.price_2 ? ` $${item.price_2}` : ""}</p>
-              </div>
-              <p className="card-content-info">{item.content ? item.content : ""}</p>
-              <div className="card-button">
-              <div className="icon-shop">
-                  <p>add to card</p>
-                </div>
-                <div className="icon-plus">
-                  <IconPlus color="#666" width="22" height="22"/>
-                </div>
-              </div>
-            </div>
-          </div>
-            )
-          })}
-        </div>
+        <ListItemProduct data={DataProduct} showLayout={showLayout}/>
       </div>
     </div>
   )
---
+++ src/Component/Shop/Product/ContainerProduct/ListItemProduct/ListItemProduct.js
@@ -0,0 +1,51 @@
+import './ListItemProduct.css';
+import IconPlus from '../../../../Images_Svg/IconPlus';
+import IconShop from '../../../../Images_Svg/IconShop';
+const ListItemProduct = ({data, showLayout}) => {
+  return (
+    <div className={showLayout && showLayout === true ? "content-layout-list" : "content-layout-grid"}>
+          {data && data.map((item, idx) => {
+            return(
+            <div className="card-product" key={idx}>
+            <div className="card-image">
+              <img alt={item.title} src={item.url ? item.url : "https://raw.githubusercontent.com/ThanhTrung05/react-hook-basic-with-yuric/main/Cat-himalayan(svg).svg"} />
+              <div className="card-image-button">
+                <div className="icon-plus">
+                  <IconPlus color="#666" width="22" height="22"/>
+                </div>
+                <div className="icon-shop">
+                  <IconShop color="#666" width="22" height="22" />
+                </div>
+              </div>
+            </div>
+            <div className="card-content-grid">
+              <h1 className="card-content-title">{item.title ? item.title : ""}</h1>
+              <div className="card-content-price">
+                <p>${item.price_2 ? `${item.price_1} - ` : item.price_1}</p>
+                <p className="card-content-line-through">{item.price_2 ? ` $${item.price_2}` : ""}</p>
+              </div>
+            </div>
+            <div className="card-content-list">
+              <h1 className="card-content-title">{item.title ? item.title : ""}</h1>
+              <div className="card-content-price">
+                <p>${item.price_2 ? `${item.price_1} - ` : item.price_1}</p>
+                <p className="card-content-line-through">{item.price_2 ? ` $${item.price_2}` : ""}</p>
+              </div>
+              <p className="card-content-info">{item.content ? item.content : ""}</p>
+              <div className="card-button">
+              <div className="icon-shop">
+                  <p>add to card</p>
+                </div>
+                <div className="icon-plus">
+                  <IconPlus color="#666" width="22" height="22"/>
+                </div>
+              </div>
+            </div>
+          </div>
+            )
+          })}
+        </div>
+  )
+}
+
+export default ListItemProduct
